bfa45eb0de95d7640097f9e064b17056525150c1
CF-Leonardo <leonardoarchetti@gmail.com>
2016-05-16 21:20:42 +0200

    uploaded file

---
+++ Carta.java
@@ -0,0 +1,20 @@
+package CartaPiùAlta;
+
+public class Carta {
+	private char segno;
+	private int valore;
+	
+public Carta(char _segno,int _valore)
+{
+	segno=_segno;
+	valore=_valore;
+}
+public int getValore()
+{
+	return valore;
+}
+public char getSegno()
+{
+	return segno;
+}
+}
